3798045208b0d7573889b98a2253ed2cb0903989
Kinza <kiinza.malik@gmail.com>
2024-09-07 15:36:41 +0500

    learning python

---
+++ python/main.py
@@ -0,0 +1,29 @@
+print('hello')
+
+# Integer
+print(4)
+
+# Float
+print(9.0)
+
+# String
+print('Kinza')
+print("Kinza")
+print('''Kinza''')
+
+# Boolean
+print(True)
+print(False)
+
+
+# List
+print([1,2,3,4,5,6])
+
+# Tuple
+print((1,2,3,4,5,6))
+
+# Set
+print({1,2,3,4,5})
+
+# Dictionary
+print({"Name":"Kinza", "Age":24, "Role":"Developer"})
